b2e173ed80c671b8a0518e32ef9abe53289dd3df
InsertBeforeFlight <joshuacrowe@outlook.com>
2021-02-20 19:00:40 +0000

    Exercise 6 Extra Credit 2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,23 +4,42 @@
 import { useState, useEffect } from 'react'
 import {fetchPokemon, PokemonInfoFallback, PokemonDataView, PokemonForm} from '../pokemon'
 
+const STATUS_IDLE = "idle";
+const STATUS_PENDING = "pending";
+const STATUS_RESOLVED = "resolved";
+const STATUS_REJECTED = "rejected";
+
+const initialState = { status: STATUS_IDLE, pokemon: null, error: null };
+
 function PokemonInfo({ pokemonName }) {
-  const [pokemon, setPokemon] = useState(null);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState(initialState);
 
   useEffect(() => {
+    setState(currentState => ({
+      ...currentState,
+      status: STATUS_PENDING
+    }))
     fetchPokemon(pokemonName)
       .then(fetchedPokemon => {
-        setPokemon(fetchedPokemon)
-        setError(null);
+        setState({
+          status: STATUS_RESOLVED,
+          pokemon: fetchedPokemon,
+          error: null
+        })
       })
-      .catch(error => setError(error));
+      .catch(error => {
+        setState({
+          status: STATUS_REJECTED,
+          pokemon: null,
+          error: error.message
+        })
+      });
   }, [pokemonName])
 
-  if (error) {
+  if (state.error) {
     return (
       <div role="alert">
-        There was an error <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+        There was an error <pre style={{ whiteSpace: "normal" }}>{state.error}</pre>
       </div>
     )
   }
@@ -29,11 +48,11 @@ function PokemonInfo({ pokemonName }) {
     return "Submit a Pokemon";
   }
 
-  if (pokemonName && !pokemon) {
+  if (pokemonName && !state.pokemon) {
     return <PokemonInfoFallback name={pokemonName} />;
   }
 
-  return <PokemonDataView pokemon={pokemon} />
+  return <PokemonDataView pokemon={state.pokemon} />
 }
 
 function App() {
